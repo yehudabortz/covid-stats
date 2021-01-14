ee2c514797786a3aa6c046e5f7116123293db9f2
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-14 12:36:42 -0600

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+require 'JSON'
+require 'net/http'
+require 'open-uri'
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require './app/controllers/application_controller'
+require_all 'app'
